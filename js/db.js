de71dd94a2c82bfb009b2dbd65102638fa3d8276
OmairaBarrera <lomaira.barrera@gmail.com>
2023-04-14 09:26:41 -0500

    feat: :sparkles: prueba

---
+++ js/db.js
@@ -0,0 +1,27 @@
+//database
+export default{
+    data:{
+        servicios:[
+            {
+                nombre: "Hunters",
+                detalle: "Espacio destinado al descanso y recreacion de nuestros campers y coworkers",
+                disponibilidad: "sujeto a horario"
+            },
+            {
+                nombre: "Cafeteria hunters",
+                detalle: "Cubiculo ubicado en el espacio de hunters pensado para ofrecer servicios de cafeteria",
+                disponibilidad: "sujeto a horario"
+            },
+            {
+                nombre: "8vo piso",
+                detalle: "Una zona ubicada en el 8vo piso que cuenta con multiples servicios de cafeteria",
+                disponibilidad: "sujeto a horario"
+            },
+            {
+                nombre: "Baños",
+                detalle: "Espacio dedicado al aseo y cuidado de los campers y coworkers",
+                disponibilidad: "uso libre"
+            },
+        ]
+    }
+}
